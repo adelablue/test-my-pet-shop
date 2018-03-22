84faae480b6093edbbac2ed868455a6935bcfe8a
Ningjing Chen <ningjingchen@NingjingdeMacBook-Air.local>
2018-03-22 17:17:44 +0800

    add app.js

--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -4,6 +4,7 @@ import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/Adoption.sol";
 
+
 contract TestAdoption {
     Adoption adoption = Adoption(DeployedAddresses.Adoption());
 
